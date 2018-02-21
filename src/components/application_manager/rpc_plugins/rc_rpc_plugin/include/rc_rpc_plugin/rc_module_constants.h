f44876063e373d58247ba6d9d686ab9a67228dec
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-21 18:45:14 +0200

    New RC modules initial implementation

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/rc_module_constants.h
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/include/rc_rpc_plugin/rc_module_constants.h
@@ -41,6 +41,14 @@ const char kclimateControlCapabilities[] = "climateControlCapabilities";
 const char kradioControlCapabilities[] = "radioControlCapabilities";
 const char kbuttonCapabilities[] = "buttonCapabilities";
 const char kseatControlCapabilities[] = "seatControlCapabilities";
+const char kaudioControlCapabilities[] = "audioControlCapabilities";
+const char klightControlCapabilities[] = "lightControlCapabilities";
+const char khmiSettingsControlCapabilities[] = "hmiSettingsControlCapabilities";
+// RemoteControlCapabilities constants
+
+const char kRadioControlData[] = "radioControlData";
+const char kClimateControlData[] = "climateControlData";
+const char kSupportedLights[] = "supportedLights";
 }  //  strings
 
 namespace result_codes {
@@ -139,6 +147,7 @@ const char kSignalStrength[] = "signalStrength";
 const char kSignalChangeThreshold[] = "signalChangeThreshold";
 const char kRadioEnable[] = "radioEnable";
 const char kState[] = "state";
+const char kSisData[] = "sisData";
 // RadioControlData struct
 
 // ClimateControlData struct
@@ -153,12 +162,25 @@ const char kDefrostZone[] = "defrostZone";
 const char kDualModeEnable[] = "dualModeEnable";
 const char kACMaxEnable[] = "acMaxEnable";
 const char kVentilationMode[] = "ventilationMode";
+const char kHeatedSteeringWheelEnable[] = "heatedSteeringWheelEnable";
+const char kHeatedWindshieldEnable[] = "heatedWindshieldEnable";
+const char kHeatedRearWindowEnable[] = "heatedRearWindowEnable";
+const char kHeatedMirrorsEnable[] = "heatedMirrorsEnable";
 // ClimateControlData struct
+
+// LightControlData
+const char kLightState[] = "lightState";
+const char kStatus[] = "status";
+const char kDensity[] = "density";
+const char kSRGBColor[] = "sRGBColor";
 
 // ModuleData struct
 const char kRadioControlData[] = "radioControlData";
 const char kClimateControlData[] = "climateControlData";
 const char kSeatControlData[] = "seatControlData";
+const char kAudioControlData[] = "audioControlData";
+const char kLightControlData[] = "lightControlData";
+const char kHmiSettingsControlData[] = "hmiSettingsControlData";
 // ModuleData struct
 
 const char kHMIAppID[] = "appID";
@@ -173,6 +195,9 @@ namespace enums_value {
 const char kClimate[] = "CLIMATE";
 const char kRadio[] = "RADIO";
 const char kSeat[] = "SEAT";
+const char kAudio[] = "AUDIO";
+const char kLight[] = "LIGHT";
+const char kHmiSettings[] = "HMI_SETTINGS";
 // ModuleType enum
 
 // RadioBand enum
